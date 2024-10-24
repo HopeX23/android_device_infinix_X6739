3a34c62645945991965793b23877534c137f40fe
bengris32 <bengris32@protonmail.ch>
2024-10-24 16:03:46 +0530

    X6739: Set SELinux to Permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,6 +50,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_MKBOOTIMG_ARGS := --base $(BOARD_KERNEL_BASE)
